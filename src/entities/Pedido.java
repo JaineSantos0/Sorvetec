5458e0c90b83608b1660d069e312e54bb812ab11
kelvin Wesley <keelvin.w@hotmail.com>
2023-02-19 14:39:28 -0300

    Adicionei os atributos e getters e setters

--- src/entities/Pedido.java
+++ src/entities/Pedido.java
@@ -1,4 +1,46 @@
 package entities;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Pedido {
+
+	private Integer numero;
+	private String nome;
+	
+	private List <Produto> listProduto = new ArrayList <>();
+
+	public Pedido(Integer numero, String nome, List<Produto> listProduto) {
+		super();
+		this.numero = numero;
+		this.nome = nome;
+		this.listProduto = listProduto;
+	}
+
+	public Integer getNumero() {
+		return numero;
+	}
+
+	public void setNumero(Integer numero) {
+		this.numero = numero;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public List<Produto> getListProduto() {
+		return listProduto;
+	}
+
+	
+	
+	
+
+
 }
+
