5b89dd28023f2a39e26ad1cc7a94a02115d69ea7
Jaine Santos <jainejosiane@gmail.com>
2023-02-22 18:01:57 -0300

    Atualização métodos de entrega

--- src/entities/Entrega.java
+++ src/entities/Entrega.java
@@ -28,27 +28,18 @@ public class Entrega {
 
 
 	public void mostrarDadosRetirada(String nome, String telefone) {
-
 		System.out.println("Tempo estimado para retirada é de 30 minutos. ");
 		System.out.println(nome + "\n" + telefone);
-		// falta imprimir o valor total sysout
 
 	}
 
 	public void mostrarDadosEntrega(String nome, String endereco, String telefone) {
-
-		double taxaEntrega = 10.00;
-		//criar uma variavel que vai receber o valor total do pedido + a taxa de entrega
-
 		System.out.println(nome + "\n" + endereco + "\n" + telefone);
 		System.out.println("Taxa de entrega R$10.00 ");
-		//imprimir valor total + entrega
 		System.out.println("O seu pedido sera entregue em até 90 minutos.");
 
 	}
 	public void pedidoFinalizado(String nome, String endereco, String telefone, String taxaEntrega) {
-
 		System.out.println(nome + "\n" + endereco + "\n" + telefone + "\n" + taxaEntrega);
-		//+ "\nValor Total: " + valorTotal/*);
 	}
 }
